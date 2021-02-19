b2e9ff3803483d0e772f2097e126be313936639a
eunyoung <eysong@autocrypt.io>
2021-02-19 10:15:34 +0900

    불필요파일 hidden 및 페이징 기능 추가

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,15 +4,48 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Post from '../components/Post'
 import Sidebar from '../components/Sidebar'
+import Pagination from "materialui-pagination-component";
 
 class IndexRoute extends React.Component {
+
+  constructor(props) {
+    super(props);
+    const totalPostCnt = props.data.allMarkdownRemark.edges ? props.data.allMarkdownRemark.edges.length : 0;
+    const totalPage = totalPostCnt > 0 ? Math.ceil(props.data.allMarkdownRemark.edges.length/5.0) : 1;
+
+    this.state = {
+      totalPostCnt,
+      totalPage,
+      currentPage: 1,
+      perPage: 5,
+      offset: 0,
+    };
+  }
+
   render() {
-    const items = []
-    const { title, subtitle } = this.props.data.site.siteMetadata
-    const posts = this.props.data.allMarkdownRemark.edges
-    posts.forEach(post => {
-      items.push(<Post data={post} key={post.node.fields.slug} />)
-    })
+    const items = [];
+    const { title, subtitle } = this.props.data.site.siteMetadata;
+    const { allMarkdownRemark } = this.props.data;
+    const posts = allMarkdownRemark ? allMarkdownRemark.edges : null;
+
+    const totalPostCnt = this.state.totalPostCnt;
+    const startIndx = this.state.offset;
+    const endIdx = startIndx + this.state.perPage;
+
+    if(posts) {
+      const displayPosts = posts.slice(startIndx, endIdx > totalPostCnt ? totalPostCnt : endIdx);
+      displayPosts.forEach(post => {
+        items.push(<Post data={post} key={post.node.fields.slug} />)
+      })
+    }
+
+
+    // const items = []
+    // const { title, subtitle } = this.props.data.site.siteMetadata
+    // const posts = this.props.data.allMarkdownRemark.edges
+    // posts.forEach(post => {
+    //   items.push(<Post data={post} key={post.node.fields.slug} />)
+    // })
 
     return (
       <Layout>
@@ -23,6 +56,32 @@ class IndexRoute extends React.Component {
           </Helmet>
           <Sidebar {...this.props} />
           <div className="content">
+            <div style={{marginTop: "15px", textAlign: "end"}}>
+              <Pagination
+                variant="text" // Valid options are ["text", "outlined"].
+                selectVariant="tab" // Valid options are ["button", "tab", "select"].
+                navigationVariant="icon" // Valid options are ["icon", "text"].
+                pageWindowVariant="ellipsis" // Valid options are ["standard", "ellipsis"].
+                color="primary" // Passed down to Material-UI components.
+                indicatorColor="primary" // Passed down to Material-UI Tabs.
+                hideNavigation={false} // Hides the first, last, previous, & next page navigation buttons.
+                hideFirst={false} // Hides the first page navigation button.
+                hideLast={false} // Hides the last page navigation button.
+                hidePrevious={false} // Hides the previous page navigation button.
+                hideNext={false} // Hides the next page navigation button.
+                disableFirst={false} // Disables the first page navigation button.
+                disableLast={false} // Disables the last page navigation button.
+                disablePrevious={false} // Disables the previous page navigation button.
+                disableNext={false} // Disables the next page navigation button.
+                page={this.state.currentPage} // The current page.
+                totalPages={this.state.totalPage} // The total number of pages.
+                elevation={0} // Passed down to Material-UI Paper component.
+                onChange={(currentPage) => {
+                  if(!currentPage) currentPage = 1;
+                  this.setState({currentPage, offset: (currentPage-1)*5})}
+                } // Callback when the page changes.
+              />
+            </div>
             <div className="content__inner">{items}</div>
           </div>
         </div>
